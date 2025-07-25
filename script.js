12dd78adcfe96adb45d1baf7a0e850fe76cf0213
Jashwanth Alwaru <112063260+Jashwnath@users.noreply.github.com>
2025-07-25 20:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+function changeHeader(){
+	var inputName = document.getElementById("name").value.trim();
+	var inputYear = document.getElementById('year').value.trim();
+
+	const params=new URLSearchParams();
+	var baseUrl = "https://localhost:8080/";
+	
+	if (inputName) params.append("name", inputName);
+	if (inputYear) params.append("year", inputYear);
+
+	
+	let finalUrl = baseUrl;
+	if([...params].length>0){
+		finalUrl += "?" + params.toString();
+	}
+	
+    document.getElementById('urlDisplay').textContent = finalUrl;
+	// Redirect to the new URL
+}
